cdefd60ef6764154950115a9ca7c768603457cb1
Stuart Sullivan <sullivanstuart94@gmail.com>
2015-02-10 13:22:43 -0500

    adding devise views to be editted

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Routes the to the homepage
+  resources :users
   root 'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
