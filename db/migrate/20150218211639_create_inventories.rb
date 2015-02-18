eea762f8391fe88d6b87afbad9c8b55a26a31e91
Wes Sak <wsak@uwaterloo.ca>
2015-02-18 17:26:20 -0500

    update schema, unfucking previous fuck ups

--- db/migrate/20150218211639_create_inventories.rb
+++ db/migrate/20150218211639_create_inventories.rb
@@ -1,7 +1,7 @@
 class CreateInventories < ActiveRecord::Migration
   def change
     create_table :inventories do |t|
-    	t.integer :part_no,			null: => false
+    	t.integer :part_no,			:null => false
     	t.integer :on_hand
     	t.integer :on_order
     	t.integer :on_hold
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218191706) do
+ActiveRecord::Schema.define(version: 20150218215620) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",           limit: 255, null: false
@@ -25,6 +25,32 @@ ActiveRecord::Schema.define(version: 20150218191706) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+
+  create_table "inventories", force: :cascade do |t|
+    t.integer  "part_no",      limit: 4, null: false
+    t.integer  "on_hand",      limit: 4
+    t.integer  "on_order",     limit: 4
+    t.integer  "on_hold",      limit: 4
+    t.integer  "inv_position", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "order_no",             limit: 4
+    t.integer  "part_no",              limit: 4
+    t.integer  "order_quantity",       limit: 4
+    t.float    "unit_cost",            limit: 24
+    t.float    "cost",                 limit: 24
+    t.float    "subtotal",             limit: 24
+    t.float    "tax",                  limit: 24
+    t.float    "total",                limit: 24
+    t.integer  "quantity_received",    limit: 4
+    t.integer  "quantity_backordered", limit: 4
+    t.string   "comment",              limit: 255
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+  end
 
   create_table "parts", force: :cascade do |t|
     t.integer  "part_no",         limit: 4,   null: false
