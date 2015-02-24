4f8b3a5920808c5c31f1622e265f7d2cdb568ee5
Stuart Sullivan <sullivanstuart94@gmail.com>
2015-02-24 13:37:55 -0500

    updated user model with the admins APIs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
 
 	before_action :correct_user, only: [:edit, :update, :show]
-	# before_action :admin_user,   only: [:index] 
+	before_action :admin_user,   only: [:index, :update_role, :remove_user] 
 
 	def index 
 		@users = User.all
+		@user = User.new
 	end
 
 	def show
@@ -24,18 +25,40 @@ class UsersController < ApplicationController
 		end
 	end
 
-	def update_role(role)
-		if current_user.update_attribute(:role, Role.find_by_name(role))	
+	def update_role
+		role = Role.find_by(params[:user][:role])
+		user = User.friendly.find(params[:user][:username])
+		if user.update_attribute(:role, role)	
 			flash[:success] = "Role Updated"
+			current_user.save
 		else
 			flash[:error] = "Role not Updated"
+		end
+	end
+
+	def remove_user
+		user = User.friendly.find(params[:user][:username])
+		if user 
+			user.destroy
+		end
+	end
+
+	def add_user
+		@user = User.new(user_params)
+		respond_to do |format|
+			if @user.save 
+				format.html { redirect_to :back, notice: "#{@user.username} has been added as a #{@user.role}" }
+				format.json { render json: @user }
+			else
+				format.html { redirect_to :back, user_notice: "#{@user.errors.count}" }
+			end
 		end
 	end
 
 	private 
 
 		def user_params
-			params.require(:user).permit(:firstname, :lastname, :email, :phonenumber, :program, :term, :year)
+			params.require(:user).permit(:username, :role_id, :firstname, :lastname, :email, :phonenumber, :program, :term, :year)
 		end
 
 		def correct_user
