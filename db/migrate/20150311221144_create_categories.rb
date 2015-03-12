203cad3635b2446afa3582124c07fccafb96109c
Wes Sak <wsak@uwaterloo.ca>
2015-03-11 21:50:12 -0400

    updated schema

--- db/migrate/20150311221144_create_categories.rb
+++ db/migrate/20150311221144_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
     	t.integer :category_id
-    	t.sring :category_name
+    	t.string :category_name
       t.timestamps null: false
     end
   end
