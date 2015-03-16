ad7267b19b2651518d1a1eedf935f93b37faf28a
gauhannah <hannahgautreau@gmail.com>
2015-03-15 22:26:46 -0400

    does this CSS make my butt look big?

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,7 @@ class AdminController < ApplicationController
 	before_filter :permision, only: :index
 
 	def index
+		@parts = Part.all
 	end
 
 	private
@@ -16,4 +17,12 @@ class AdminController < ApplicationController
 		def manager 
 			redirect_to(root_url) unless current_user.manager?
 		end
+
+ 		def sort_column 
+ 			%w[part_number description price].include?(params[:sort]) ? params[:sort] : "part_number"
+ 		end
+
+ 		def sort_direction
+ 			%w[asc desc].include?(params[:direction]) ? params[:direction] : "asc" 
+ 		end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :sort_column, :sort_direction
+
+
+  private
+ 		def sort_column 
+ 			%w[part_number description price].include?(params[:sort]) ? params[:sort] : "part_number"
+ 		end
+
+ 		def sort_direction
+ 			%w[asc desc].include?(params[:direction]) ? params[:direction] : "asc" 
+ 		end
 end
